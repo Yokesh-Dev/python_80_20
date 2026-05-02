e5e212ccae6ffd2fe297be75caf8f5bae6a885d0
Yokesh-Dev <166229512+Yokesh-Dev@users.noreply.github.com>
2026-05-02 21:37:38 +0530

    Implement arithmetic operations and age calculation
    Added basic arithmetic operations and age calculation.

--- challenges.py
+++ challenges.py
@@ -1,1 +1,15 @@
+# 1
+num1 = 15
+num2 = 13
 
+print(num1 + num2)   
+print(num1 - num2)  
+print(num1 * num2)   
+print(num1 / num2)   
+
+# 2
+user_year = int(input("Enter your year of birth: "))
+current_year = 2026
+user_age = current_year - user_year
+
+print("You are", user_age, "years old.")
