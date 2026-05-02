82de4975f8333dc26ea06450c9e8ff20a4359a55
Yokesh-Dev <166229512+Yokesh-Dev@users.noreply.github.com>
2026-05-02 21:52:49 +0530

    Update challenges.py

--- challenges.py
+++ challenges.py
@@ -13,3 +13,28 @@ current_year = 2026
 user_age = current_year - user_year
 
 print("You are", user_age, "years old.")
+
+
+# 1
+num = 12
+floatnum = 11.3
+name = "str"
+status = True
+
+print(num, type(num))
+print(floatnum, type(floatnum))
+print(name, type(name))
+print(status, type(status))
+
+# 2 
+user_input = int(input("Enter a Number "))
+user_input2 = int(input("Enter another Number "))
+
+print(user_input + user_input2 , user_input2 * user_input)
+
+# 3
+lists = [11,12,23,24]
+lists.append(25)
+print(lists , len(lists))
+lists.remove(11)
+print(lists , len(lists))
